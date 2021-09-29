b4e46b1f43a30512d1dd39a743868e4b572a536a
alessandrothedev <spiridigliozzi.alessandro@gmail.com>
2021-09-29 14:29:50 +0100

    added questions object

---
+++ questions/questions.js
@@ -0,0 +1,74 @@
+export const questions = [
+    {
+        question: 'How many stripes are there on the US flag?',
+        answers: [
+            {quizAnswer: '13', correct: true},
+            {quizAnswer: '11', correct: false},
+            {quizAnswer: '15', correct: false},
+            {quizAnswer: '51', correct: false}
+        ]
+    },
+    {
+        question: 'Which of the following empires had no written language?',
+        answers: [
+            {quizAnswer: 'Roman', correct: false},
+            {quizAnswer: 'Aztec', correct: false},
+            {quizAnswer: 'Egyptian', correct: false},
+            {quizAnswer: 'Incan', correct: true}
+        ]
+    },
+    {
+        question: 'What’s the capital of Canada?',
+        answers: [
+            {quizAnswer: 'Vancouver', correct: false},
+            {quizAnswer: 'Ottawa', correct: true},
+            {quizAnswer: 'Toronto', correct: false},
+            {quizAnswer: 'Calgary', correct: false}
+        ]
+    },
+    {
+        question: 'Which famous graffiti artist comes from Bristol?',
+        answers: [
+            {quizAnswer: 'Picasso', correct: false},
+            {quizAnswer: 'Andy Warhol', correct: false},
+            {quizAnswer: 'Dauchamp', correct: false},
+            {quizAnswer: 'Banksy', correct: true}
+        ]
+    },
+    {
+        question: 'When did they open the London underground?',
+        answers: [
+            {quizAnswer: '1798', correct: false},
+            {quizAnswer: '1863', correct: true},
+            {quizAnswer: '1912', correct: false},
+            {quizAnswer: '1892', correct: false}
+        ]
+    },
+    {
+        question: 'When was Netflix founded?',
+        answers: [
+            {quizAnswer: '2001', correct: false},
+            {quizAnswer: '1998', correct: false},
+            {quizAnswer: '2009', correct: false},
+            {quizAnswer: '1997', correct: true}
+        ]
+    },
+    {
+        question: 'Which 80s star is recognised by Guinness World Records as the best-selling female recording artist of all time? ',
+        answers: [
+            {quizAnswer: 'Cher', correct: false},
+            {quizAnswer: 'Aretha Franklin', correct: false},
+            {quizAnswer: 'Madonna', correct: true},
+            {quizAnswer: 'Celine Dion', correct: false}
+        ]
+    },
+    {
+        question: 'Founded as Albrecht Discount, what is the company known as now?',
+        answers: [
+            {quizAnswer: 'Tesco', correct: false},
+            {quizAnswer: 'Poundland', correct: false},
+            {quizAnswer: 'Waitrose', correct: false},
+            {quizAnswer: 'Aldi', correct: true}
+        ]
+    }
+]
